9eb234358a14fefa6a24824d5f23a9d90fd6ccdb
seangao <sean@nbp-18-225.nbp.ruw.rutgers.edu>
2015-10-03 18:39:03 -0400

    HTML template

--- main.php
+++ main.php
@@ -2,12 +2,22 @@
 <html>
 	<head>
 		<title>Quit Your Addiction!</title>
-		<script src="../js/addictionHelper.js"></script>
-		<link rel="stylesheet" href="css/style.css">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
 	<body>
-		<?php
-			echo "Hello world!";
-		?>
+		<section id="banner">
+			<h2><strong>Stop</strong> your addiction</h2>
+			<ul class="actions">
+				<li><a href="#" class="button special">Get started</a></li>
+			</ul>
+		</section>
+
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/js/addictionHelper.js"></script>
 	</body>
 </html>
