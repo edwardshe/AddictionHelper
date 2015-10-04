17278a9edce345218cfa9c6467c47db87c3b8ed6
seangao <seancty@gmail.com>
2015-10-03 21:32:52 -0400

    register submit

--- main.php
+++ main.php
@@ -22,15 +22,15 @@
 			</div>
 		</section>
 
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/js/skel.min.js"></script>
-		<script src="assets/js/util.js"></script>
-		<script src="assets/js/main.js"></script>
-
 		<footer id="footer">
 			<div class="copyright">
 				Design: <a href="http://templated.co/">TEMPLATED</a>.
 			</div>
 		</footer>
+
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
 	</body>
 </html>
