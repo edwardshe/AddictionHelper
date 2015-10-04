8f77ef99c32cf4abeb18d5447defe7a6f1680e17
seangao <seancty@gmail.com>
2015-10-04 00:41:11 -0400

    working login

--- login.php
+++ login.php
@@ -1,10 +1,28 @@
 <?php
-$email = "";
-$password = "";
-
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-	$name = test_input($_POST["email"]);
-	$password = test_input($_POST["password"]);
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+
+	$dbh = new PDO('sqlite:info/login.db');
+	$stmt = $dbh->prepare("SELECT * FROM login WHERE email = ? ORDER BY rowid DESC LIMIT 1");
+	$stmt->bindParam(1, $email);
+	$stmt->execute();
+	$result = $stmt->fetchAll();
+	foreach ($result as $val) {
+		$p = $val['password'];
+	}
+
+
+	if (isset($_COOKIE['alevior'])) {
+		$c = $_COOKIE['alevior'];
+		if (hash('sha512', $c) == $p) header('Location: home.php');
+	} elseif (isset($_POST['password'])){
+		if (hash('sha512', $password) == $p) {
+			$hour = time() + 60 * 60;
+			setcookie('alevior', $_POST['password'], $hour);
+			header('Location: home.php');
+		}
+	}
 }
 ?>
 
@@ -24,6 +42,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 		</section>
 		<section id="one" class = "wrapper">
 			<div class="inner">
+				<p><?php echo hash('sha512', $password)?></p>
+				<p><?php echo $p?></p>
 				<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 					<div class="row uniform">
 						<div class="6u$ 12u$(xsmall)">
